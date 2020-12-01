16df41e8fc760dec4afd145f36b7d037b8e17737
matte1206 <40821822+matte1206@users.noreply.github.com>
2020-12-01 08:25:58 +0100

    added function

--- Verschluesslung/Caeser/caeser93.java
+++ Verschluesslung/Caeser/caeser93.java
@@ -41,6 +41,33 @@ public class caeser93 extends JFrame implements ActionListener {
         return chiffre;
     }
 
+    String encryptTrithemius(String txt) {
+        String chiffre = "";
+        char ch;
+        int len = txt.length();
+        int charNum;
+        int codeWordNum = 0;
+
+        for(int i = 0; i < len; i++){
+            ch = txt.charAt(i);
+
+            key = i;
+
+            if(ch != 32){
+                charNum = (int) ch - 65;
+                charNum += key;
+                charNum %= 26;
+
+                chiffre += (char) (charNum + 65);
+            }else{
+                if(ch == 32) {chiffre += " ";}  //Leerzeichen
+            }
+
+        }
+
+        return chiffre;
+    }
+
     String removeVowelMutation(String input) {
         String txt = "";
         char ch;
@@ -58,14 +85,21 @@ public class caeser93 extends JFrame implements ActionListener {
     }
 
     JPanel jpnl = new JPanel();
+    JPanel btnJpnl = new JPanel();
     JLabel topKeyLbl = new JLabel("Geben Sie das Verschluesslungswort ein:");
     JLabel topInLbl = new JLabel("Geben Sie das Codewort ein:");
     JTextField keyTxt = new JTextField(15);
     JTextField inTxt = new JTextField(15);
     JTextField chiffreTxt = new JTextField(15);
+    JButton encryptBtn = new JButton("Vigenere");
+    JButton encryptTrithemiusBtn = new JButton("Trithemius");
 
     public caeser93(String title) {
         super(title);
+
+        btnJpnl.setLayout(new BoxLayout(btnJpnl, BoxLayout.X_AXIS));
+        btnJpnl.add(encryptBtn);
+        btnJpnl.add(encryptTrithemiusBtn);
 
         jpnl.setLayout(new BoxLayout(jpnl, BoxLayout.Y_AXIS));
         jpnl.add(topKeyLbl);
@@ -73,11 +107,15 @@ public class caeser93 extends JFrame implements ActionListener {
         jpnl.add(topInLbl);
         jpnl.add(inTxt);
         jpnl.add(chiffreTxt);
+        jpnl.add(btnJpnl);
 
         chiffreTxt.setEditable(false);
 
-        keyTxt.addActionListener(this);
-        inTxt.addActionListener(this);
+        encryptBtn.setActionCommand("Vignere");
+        encryptTrithemiusBtn.setActionCommand("Trithemius");
+
+        encryptBtn.addActionListener(this);
+        encryptTrithemiusBtn.addActionListener(this);
 
         this.getContentPane().add(jpnl);
         pack();
@@ -86,15 +124,30 @@ public class caeser93 extends JFrame implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent evt) {
-        if(keyTxt.getText() != "" && inTxt.getText() != ""){
-            String input = inTxt.getText().toUpperCase();
-            String codeWord = keyTxt.getText().toUpperCase();
+        String actionCmd = evt.getActionCommand();
 
-            input = removeVowelMutation(input);
+        key = 3;
 
-            keyTxt.setText(codeWord);
-            inTxt.setText(input);
-            chiffreTxt.setText(encrypt(input, codeWord));
+        if(actionCmd.equals("Vignere")) {
+            if(keyTxt.getText() != "" && inTxt.getText() != ""){
+                String input = inTxt.getText().toUpperCase();
+                String codeWord = keyTxt.getText().toUpperCase();
+
+                input = removeVowelMutation(input);
+
+                keyTxt.setText(codeWord);
+                inTxt.setText(input);
+                chiffreTxt.setText(encrypt(input, codeWord));
+            } else {}
+        } else if(actionCmd.equals("Trithemius")) {
+            if(inTxt.getText() != "") {
+                String input = inTxt.getText().toUpperCase();
+
+                input = removeVowelMutation(input);
+
+                inTxt.setText(input);
+                chiffreTxt.setText(encryptTrithemius(input));
+            } else {}
         }
     }
 
